9bcd78e23d656cb20d9fc85b5b9ecb060eaafc40
Alan Mitchell <mitchellalan1999@icloud.com>
2025-08-09 21:39:17 -0500

    update: modified file to work with interval.zig

--- src/hit.zig
+++ src/hit.zig
@@ -1,31 +1,41 @@
 const std = @import("std");
+
 const vec3 = @import("vec.zig").Vec3;
 const ray = @import("ray.zig").Ray;
 const sphere = @import("sphere.zig").Sphere;
+const interval = @import("interval.zig").Interval;
 
 // struct to hold record of hit, very straightforward
 pub const HitRecord = struct {
     p: vec3,
     normal: vec3,
     t: f64,
+    front_face: bool,
 
     pub fn init() HitRecord {
         return HitRecord {
             .p = vec3.zero(),
             .normal = vec3.zero(),
             .t = 0.0,
+            .front_face = false,
         };
+    }
+
+    pub fn set_face_normal(self: *HitRecord, r: ray, outward_normal: vec3) void {
+        self.front_face = r.direction().dot(outward_normal) < 0;
+        self.normal = if (self.front_face) outward_normal else outward_normal.scale(-1.0);
     }
 };
 
-// 
+// tagged union for future expansion of other shapes maybe
+// allows to keep all the shapes under one variable but check the tag when hit is called
+// only sphere for now
 pub const Hittable = union(enum) {
     sphere: sphere,
 
-    pub fn hit(self: Hittable, r: ray, ray_tmin: f64, ray_tmax: f64, rec: *HitRecord) bool {
+    pub fn hit(self: Hittable, r: ray, ray_t: interval, rec: *HitRecord) bool {
         switch (self) {
-            .sphere => |_sphere| return _sphere.hit(r, ray_tmin, ray_tmax, rec),
+            .sphere => |_sphere| return _sphere.hit(r, ray_t, rec),
         }
     }
 };
-
