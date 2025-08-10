0bbe3efe06ebc89516d2016964ace069e2559b10
Alan Mitchell <mitchellalan1999@icloud.com>
2025-08-09 21:42:57 -0500

    add: added interval.zig to make codebase more concise

---
+++ src/interval.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub const Interval = struct {
+    min: f64,
+    max: f64,
+
+    pub const empty = Interval {
+        .min = std.math.inf(f64),
+        .max = -std.math.inf(f64),
+    };
+
+    pub const universe = Interval {
+        .min = -std.math.inf(f64),
+        .max = std.math.inf(f64),
+    };
+
+    pub fn init() Interval {
+        return Interval.empty;
+    }
+
+    pub fn new(min: f64, max: f64) Interval {
+        return Interval {
+            .min = min,
+            .max = max,
+        };
+    }
+
+    pub fn size(self: Interval) f64 {
+        return self.max - self.min;
+    }
+
+    pub fn contains(self: Interval, x: f64) bool {
+        return self.min <= x and x <= self.max;
+    }
+
+    pub fn surrounds(self: Interval, x: f64) bool {
+        return self.min < x and x < self.max;
+    }
+};
