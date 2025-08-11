994d8c374587a0d65360524fb26fdb35dd13f3ae
Alan Mitchell <mitchellalan1999@icloud.com>
2025-08-11 09:41:50 -0500

    updated: now able to render final image from Ray Tracing In One Weekend

--- src/main.zig
+++ src/main.zig
@@ -3,36 +3,119 @@ const std = @import("std");
 const vec3 = @import("vec.zig").Vec3;
 const color = @import("color.zig");
 const ray = @import("ray.zig").Ray;
-const hit = @import("hit.zig");
-const hit_record = hit.HitRecord;
-const hittable = hit.Hittable;
+const hit_record = @import("hit.zig").HitRecord;
+const hittable = @import("hit.zig").Hittable;
 const hittables = @import("hittables.zig").Hittables;
 const sphere = @import("sphere.zig").Sphere;
 const interval = @import("interval.zig").Interval;
 const camera = @import("camera.zig").Camera;
+const material = @import("material.zig");
+const random = @import("utils.zig").Random;
 
 pub fn main() !void {
     var world = hittables.init(std.heap.page_allocator);
     defer world.deinit();
 
-    const sphere1 = sphere.init(vec3.new(0, 0, -1), 0.5);
-    const sphere2 = sphere.init(vec3.new(0, -100.5, -1), 100);
+    var material_list = std.ArrayList(material.Material).init(std.heap.page_allocator);
+    defer material_list.deinit();
+    try material_list.ensureTotalCapacity(500);
 
+    try material_list.append(material.Material {
+        .lambertian = material.Lambertian.init(vec3.new(0.5, 0.5, 0.5))     
+    });     
+    const ground_material = &material_list.items[material_list.items.len - 1];
+    
     try world.add(hittable {
-        .sphere = sphere1,
+        .sphere = sphere.init(vec3.new(0, -1000, 0), 1000, ground_material)
     });
 
-    try world.add(hittable {
-        .sphere = sphere2,
+    var a: i32 = -11;
+    while (a < 11) : (a += 1) {
+        var b: i32 = -11;
+        
+        while (b < 11) : (b += 1) {
+            const choose_mat = random.random_number(&random.rng);
+            const center = vec3.new(
+                @as(f64, @floatFromInt(a)) + 0.9 * random.random_number(&random.rng),
+                0.2,
+                @as(f64, @floatFromInt(b)) + 0.9 * random.random_number(&random.rng)
+            );
+
+            if (center.subtract(vec3.new(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {
+                    // Diffuse
+                    const albedo = vec3.random().multiply(vec3.random());
+                    try material_list.append(material.Material {
+                        .lambertian = material.Lambertian.init(albedo) 
+                    });
+
+                    const sphere_material = &material_list.items[material_list.items.len - 1];
+                    try world.add(hittable {
+                        .sphere = sphere.init(center, 0.2, sphere_material)
+                    });
+                } else if (choose_mat < 0.95) {
+                    // Metal
+                    const albedo = vec3.random_within_range(0.5, 1);
+                    const fuzz = random.random_range(&random.rng, 0, 0.5);
+                    try material_list.append(material.Material { 
+                        .metal = material.Metal.init(albedo, fuzz) 
+                    });
+
+                    const sphere_material = &material_list.items[material_list.items.len - 1];
+                    try world.add(hittable { 
+                        .sphere = sphere.init(center, 0.2, sphere_material) 
+                    });
+                } else {
+                    // Glass
+                    try material_list.append(material.Material { 
+                        .dielectric = material.Dielectric.init(1.5) 
+                    });
+
+                    const sphere_material = &material_list.items[material_list.items.len - 1];
+                    try world.add(hittable { 
+                        .sphere = sphere.init(center, 0.2, sphere_material) 
+                    });
+                }
+            }
+        }
+    }
+
+    // three large spheres
+    try material_list.append(material.Material { 
+        .dielectric = material.Dielectric.init(1.5) 
+    });
+    const mat1 = &material_list.items[material_list.items.len - 1];
+    try world.add(hittable { 
+        .sphere = sphere.init(vec3.new(0, 1, 0), 1.0, mat1) 
+    });
+
+    try material_list.append(material.Material { 
+        .lambertian = material.Lambertian.init(vec3.new(0.4, 0.2, 0.1)) 
+    });
+    const mat2 = &material_list.items[material_list.items.len - 1];
+    try world.add(hittable { 
+        .sphere = sphere.init(vec3.new(-4, 1, 0), 1.0, mat2) 
+    });
+
+    try material_list.append(material.Material { 
+        .metal = material.Metal.init(vec3.new(0.7, 0.6, 0.5), 0.0) 
+    });
+    const mat3 = &material_list.items[material_list.items.len - 1];
+    try world.add(hittable { 
+        .sphere = sphere.init(vec3.new(4, 1, 0), 1.0, mat3) 
     });
 
     var cam = camera {
-        .image_height = undefined,
-        .center = undefined,
-        .pixel_00_location = undefined,
-        .pixel_delta_u = undefined,
-        .pixel_delta_v = undefined,
-        .pixel_samples_scale = undefined,
+        .aspect_ratio = 16.0 / 9.0,
+        .image_width = 1200,
+        .samples_per_pixel = 250,
+        .max_depth = 50,
+        .vfov = 20,
+        .look_from = vec3.new(13, 2, 3),
+        .look_at = vec3.zero(),
+        .vup = vec3.new(0, 1, 0),
+        .defocus_angle = 0.6,
+        .focus_dist = 10.0,
     };
 
     try cam.render(&world);
